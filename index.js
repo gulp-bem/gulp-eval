9448969cf3f9e5d27137b7fcde951ea8e656f9e5
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-04-14 02:39:45 +0300

    Change runInNewContext to inThisContext

--- index.js
+++ index.js
@@ -1,25 +1,89 @@
 var through = require('through2');
 var vm = require('vm');
+var path = require('path');
 var Module = require('module');
 
 module.exports = function(context) {
-	return through.obj(function(file, enc, next) {
-		var fileContent = file.contents.toString(enc);
-		file.data = _eval(fileContent, file.relative, context);
-		next(null, file);
-	});
+    return through.obj(function(file, enc, next) {
+        var fileContent = file.contents.toString(enc);
+        file.data = _eval(fileContent, file.path, context);
+        next(null, file);
+    });
 };
 
 function _eval(content, filename, context) {
+    var dirname = path.dirname(filename);
+    var ext = path.extname(filename);
+
+    content = stripBOM(content);
+
+    if(ext === '.json') {
+        return tryCatch(JSON.parse.bind(null, content), function(err) {
+            err.message = filename + ': ' + err.message;
+            throw err;
+        });
+    }
+
     var sandbox = {};
 
     sandbox.module = new Module(filename, module.parent);
-    sandbox.exports = sandbox.module.exports;
+    sandbox.exports = sandbox.module.exports = {'__42__': '__42__'};
     sandbox.require = sandbox.module.require;
-	Object.assign(sandbox, context);
 
-    var res = vm.runInThisContext(content, {filename: filename});
-    return res || sandbox.module.exports;
+    var args = [sandbox.exports, sandbox.require, sandbox.module, filename, dirname];
+    context && (context = Object.keys(context).map(function(key) {
+        args.push(context[key]);
+        return key;
+    }));
+
+    var wrapper = wrap(content, context);
+    var options = {filename: filename, lineOffset: 0, displayErrors: true};
+    var compiledWrapper = vm.runInThisContext(wrapper, options);
+
+    compiledWrapper.apply(sandbox.exports, args);
+
+    var result;
+    if(sandbox.module.exports['__42__'] && Object.keys(sandbox.module.exports).length === 1) {
+        result = vm.runInThisContext(content);
+    } else {
+        delete sandbox.module.exports['__42__'];
+        result = sandbox.module.exports;
+    }
+    return result;
 }
 
 module.exports.eval = _eval;
+
+function wrap(body, extKeys) {
+    var wrapper = [
+        '(function (exports, require, module, __filename, __dirname',
+        ') { ',
+        '\n});'
+    ];
+
+    extKeys = extKeys ? extKeys.map(function(key) {
+        return ', ' + key;
+    }) : '';
+
+    return wrapper[0] + extKeys + wrapper[1] + body + wrapper[2];
+}
+
+function tryCatch(fn, cb) {
+    try {
+        return fn();
+    } catch(e) {
+        cb(e);
+    }
+}
+
+/**
+ * Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
+ * because the buffer-to-string conversion in `fs.readFileSync()`
+ * translates it to FEFF, the UTF-16 BOM.
+ */
+function stripBOM(content) {
+    if(content.charCodeAt(0) === 0xFEFF) {
+        content = content.slice(1);
+    }
+    return content;
+}
--- test/test.js
+++ test/test.js
@@ -3,18 +3,6 @@ var gulpUtil = require('gulp-util');
 
 var gulpEval = require('../');
 
-var moduleContent =
-`module.exports = {
-    block: 'page',
-    content: 'hello'
-}`;
-
-var evalContent =
-`({
-    block: 'page',
-    content: 'hello'
-})`;
-
 it('should eval simple expression', function(done) {
     var stream = gulpEval();
 
@@ -47,6 +35,34 @@ it('should not eval simple object', function(done) {
     stream.end();
 });
 
+var moduleContent =
+`module.exports = {
+    block: 'page',
+    content: 'hello'
+}`;
+
+it('should eval module.exports', function(done) {
+    var stream = gulpEval();
+
+    stream.on('data', function(file) {
+        expect(file.data.block).to.equal('page');
+    });
+    stream.on('end', done);
+
+    stream.write(new gulpUtil.File({
+        path: 'blocks/button.js',
+        contents: new Buffer(moduleContent)
+    }));
+
+    stream.end();
+});
+
+var exportsContent =
+`exports = {
+    block: 'page',
+    content: 'hello'
+}`;
+
 it('should eval module', function(done) {
     var stream = gulpEval();
 
@@ -56,12 +72,85 @@ it('should eval module', function(done) {
     stream.on('end', done);
 
     stream.write(new gulpUtil.File({
-        path: 'file.js',
-        contents: new Buffer(moduleContent)
+        path: 'blocks/button.js',
+        contents: new Buffer(exportsContent)
     }));
 
     stream.end();
 });
+
+var moduleExportsContent =
+`exports = module.exports = {
+    block: 'page',
+    content: 'hello'
+}`;
+
+it('should eval exports = module.exports', function(done) {
+    var stream = gulpEval();
+
+    stream.on('data', function(file) {
+        expect(file.data.block).to.equal('page');
+    });
+    stream.on('end', done);
+
+    stream.write(new gulpUtil.File({
+        path: 'blocks/button.js',
+        contents: new Buffer(moduleExportsContent)
+    }));
+
+    stream.end();
+});
+
+var moduleExportsChangeField =
+`exports.block = 'Fuck';
+`;
+
+it('should eval exports part', function(done) {
+    var stream = gulpEval();
+
+    stream.on('data', function(file) {
+        expect(file.data.block).to.equal('Fuck');
+    });
+    stream.on('end', done);
+
+    stream.write(new gulpUtil.File({
+        path: 'blocks/button.js',
+        contents: new Buffer(moduleExportsChangeField)
+    }));
+
+    stream.end();
+});
+
+var requireContent =
+`
+var p = require('../package.json');
+module.exports = {
+    block: p.name,
+    content: 'hello'
+}
+`;
+
+it('should eval module with require', function(done) {
+    var stream = gulpEval();
+
+    stream.on('data', function(file) {
+        expect(file.data.block).to.equal('gulp-eval');
+    });
+    stream.on('end', done);
+
+    stream.write(new gulpUtil.File({
+        path: 'blocks/button.js',
+        contents: new Buffer(requireContent)
+    }));
+
+    stream.end();
+});
+
+var evalContent =
+`({
+    block: 'page',
+    content: 'hello'
+})`;
 
 it('should eval object', function(done) {
     var stream = gulpEval();
@@ -79,10 +168,39 @@ it('should eval object', function(done) {
     stream.end();
 });
 
-var globalObjectsContent = `module.exports = typeof console === 'object'`;
+it('should eval json', function(done) {
+    var stream = gulpEval();
+
+    stream.on('data', function(file) {
+        expect(file.data.block).to.equal('page');
+    });
+    stream.on('end', done);
+
+    stream.write(new gulpUtil.File({
+        path: 'file.json',
+        contents: new Buffer('{"block":"page"}')
+    }));
+
+    stream.end();
+});
+
+it('should throw on bad json', function() {
+    var stream = gulpEval();
+    expect(function() {
+        stream.write(new gulpUtil.File({
+            path: 'file.json',
+            contents: new Buffer('{"block":"page",}')
+        }));
+    }).to.throw(Error);
+
+    stream.end();
+});
+
+var globalObjectsContent = `module.exports = typeof boo === 'object'`;
 
 it('should provide global objects', function(done) {
-    var stream = gulpEval({console});
+    var boo = {};
+    var stream = gulpEval({boo});
 
     stream.on('data', function(file) {
         expect(file.data).to.be.true;
